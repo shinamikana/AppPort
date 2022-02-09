954ec9aa77d94ca97a4389f951020a580eecb9b3
shinami <nekonaitoway@yahoo.co.jp>
2022-02-09 19:14:57 +0900

    heroku mbstring setting

--- memoData.php
+++ memoData.php
@@ -7,17 +7,18 @@ $date = date("Y/m/d H:i:s");
 //memoがポストされたなら
 if (isset($_POST['text'])) {
   $text = trim($_POST['text'] ?? '');
-  echo mb_strlen($text);
-  $memoPost = $mysqli->prepare('INSERT INTO memo(text,date,user_id) VALUES(?,?,?)');
-  $memoPost->bind_param('ssi', $text, $date, $_SESSION['id']);
-  $memoPost->execute();
-  $memoPost->close();
-  $insert = $mysqli->insert_id;
-  $text = h($text);
-  $data = array('text' => $text, 'insert' => $insert, 'date' => $date);
-  header("Content-type: application/json; charset=UTF-8");
-  echo json_encode($data);
-  exit;
+  if (mb_strlen($text)) {
+    $memoPost = $mysqli->prepare('INSERT INTO memo(text,date,user_id) VALUES(?,?,?)');
+    $memoPost->bind_param('ssi', $text, $date, $_SESSION['id']);
+    $memoPost->execute();
+    $memoPost->close();
+    $insert = $mysqli->insert_id;
+    $text = h($text);
+    $data = array('text' => $text, 'insert' => $insert, 'date' => $date);
+    header("Content-type: application/json; charset=UTF-8");
+    echo json_encode($data);
+    exit;
+  }
 }
 
 //メモ削除
